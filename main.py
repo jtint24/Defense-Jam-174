9afb4804965b0c0cd2ee5f5d1d31d94c925cf05d
Joshua Tint <tintjosh@gmail.com>
2024-12-20 16:41:40 -0700

    add conflict logic

--- main.py
+++ main.py
@@ -30,13 +30,14 @@ class Team(Enum):
 
 
 class Unit:
-    def __init__(self, type: UnitType, direction: Direction,  team: Team):
+    def __init__(self, type: UnitType, direction: Direction, team: Team):
         self.type = type
         self.direction = direction
         self.team = team
+        self.defense = 1
 
     def get_image(self):
-        return self.team.value[self.type.value-1]
+        return self.team.value[self.type.value - 1]
 
 
 class TileTypeData(NamedTuple):
@@ -57,9 +58,10 @@ class TileType(Enum):
 
 
 class Tile:
-    def __init__(self, type: TileType, unit: Optional[Unit]):
+    def __init__(self, type: TileType, unit: Optional[Unit], placeable: bool = True):
         self.type = type
         self.unit = unit
+        self.placeable = placeable
 
     def is_free(self) -> bool:
         "Returns whether the tile is clear to walk on, based on tile type and other units on it"
@@ -116,7 +118,7 @@ class Board:
             unit_line = []
             for col_idx, tile in enumerate(row):
 
-                if tile.unit is not None:
+                if tile.unit is not None and (len(unit_line) == 0 or tile.unit.team == unit_line[0].team):
                     unit_line.append(tile.unit)
                 else:
                     for unit in unit_line:
@@ -124,8 +126,72 @@ class Board:
                     unit_line = []
             for unit in unit_line:
                 unit.type = UnitType(min(3, len(unit_line)))
-    def resolve_conflict(self, row_idx: int, col_idx: int):
-        pass
+
+        # And we update each troop's defense
+
+        for col_idx in range(len(self.tiles[0])):
+            unit_line = []
+            for row_idx in range(len(self.tiles)):
+                tile = self.tiles[row_idx][col_idx]
+
+                if tile.unit is not None and (len(unit_line) == 0 or tile.unit.team == unit_line[0].team):
+                    unit_line.append(tile.unit)
+                else:
+                    for unit in unit_line:
+                        unit.defense = min(5, len(unit_line))
+                    unit_line = []
+            for unit in unit_line:
+                unit.defense = min(5, len(unit_line))
+
+    def resolve_conflict(self, conflict: "Conflict") -> Set[Tuple[int, int]]:
+        team_damage = {team: 0 for team in Team}
+
+        sorted_belligerents = sorted(
+            conflict.belligerent_coordinates,
+            key=lambda coordinate: self.tiles[coordinate[0]][coordinate[1]].unit.defense,
+            reverse=True
+        )
+
+        for b_row_idx, b_col_idx in sorted_belligerents:
+            unit = self.tiles[b_row_idx][b_col_idx].unit
+            for team in Team:
+                if team != unit.team:
+                    team_damage[team] += unit.type.value
+
+        # print(team_damage)
+
+        survivors = set()
+        weakest_unit_coordinates = []
+        weakest_unit_damage = -1
+
+        for b_row_idx, b_col_idx in sorted_belligerents:
+
+            unit = self.tiles[b_row_idx][b_col_idx].unit
+
+            # print("remaining damage", team_damage[unit.team])
+
+            # print("unit team", unit.team, "unit defense", unit.defense, "unit class", unit.type.value)
+
+            exact_damage = team_damage[unit.team] / unit.defense
+            # print("exact damage", exact_damage)
+            damage = team_damage[unit.team] // unit.defense
+
+            team_damage[unit.team] -= unit.defense
+            team_damage[unit.team] = max(team_damage[unit.team], 0)
+
+            if exact_damage > weakest_unit_damage:
+                weakest_unit_coordinates = [(b_row_idx, b_col_idx)]
+                weakest_unit_damage = exact_damage
+            elif exact_damage == weakest_unit_damage:
+                weakest_unit_coordinates.append((b_row_idx, b_col_idx))
+
+            if damage == 0:
+                survivors.add((b_row_idx, b_col_idx))
+
+        if len(survivors) == len(sorted_belligerents):
+            survivors.difference_update(weakest_unit_coordinates)
+
+        return survivors
 
     def move_unit(self, new_tiles: List[List[Tile]], row_idx: int, col_idx: int):
         _, f_row_idx, f_col_idx = self.get_faced_tile(row_idx, col_idx)
@@ -175,8 +241,10 @@ class Board:
                                 else:
                                     if (f_row_idx, f_col_idx) not in same_team_conflicts:
                                         same_team_conflicts[(f_row_idx, f_col_idx)] = self.Conflict(set())
-                                    same_team_conflicts[(f_row_idx, f_col_idx)].belligerent_coordinates.add((row_idx, col_idx))
-                                    same_team_conflicts[(f_row_idx, f_col_idx)].belligerent_coordinates.update(other_claimants)
+                                    same_team_conflicts[(f_row_idx, f_col_idx)].belligerent_coordinates.add(
+                                        (row_idx, col_idx))
+                                    same_team_conflicts[(f_row_idx, f_col_idx)].belligerent_coordinates.update(
+                                        other_claimants)
                         if (f_row_idx, f_col_idx) not in squares_claimed_by_team[tile.unit.team]:
                             squares_claimed_by_team[tile.unit.team][(f_row_idx, f_col_idx)] = []
 
@@ -206,7 +274,8 @@ class Board:
                         combined_with_conflict = False
                         for belligerent_point in passing_conflict.belligerent_coordinates:
                             if belligerent_point in conflicts:
-                                conflicts[belligerent_point].belligerent_coordinates.update(passing_conflict.belligerent_coordinates)
+                                conflicts[belligerent_point].belligerent_coordinates.update(
+                                    passing_conflict.belligerent_coordinates)
                                 combined_with_conflict = True
                                 break
                         if not combined_with_conflict:
@@ -214,8 +283,14 @@ class Board:
 
         # Resolve all conflicts
 
+        for _, conflict in conflicts.items():
+            survivor_coordinates = self.resolve_conflict(conflict)
+            for b_row_idx, b_col_idx in conflict.belligerent_coordinates:
+                if (b_row_idx, b_col_idx) not in survivor_coordinates:
+                    self.tiles[b_row_idx][b_col_idx].unit = None
 
         # Iterate over all units on the board, finding each ones' chain.
+
         for row_idx, row in enumerate(self.tiles):
             for col_idx, tile in enumerate(row):
                 if (row_idx, col_idx) in identified_unit_points:
@@ -240,14 +315,17 @@ class Board:
 
         return list(chains_starting_points.values()), locked_unit_points
 
-    def get_passing_conflict(self, opponent: Tuple[int, int], opp_claimed_square: Tuple[int, int], squares_claimed_by_team: Dict[str, Dict[Tuple[int, int], List[Tuple[int, int]]]]) -> Optional[Conflict]:
+    def get_passing_conflict(self, opponent: Tuple[int, int], opp_claimed_square: Tuple[int, int],
+                             squares_claimed_by_team: Dict[str, Dict[Tuple[int, int], List[Tuple[int, int]]]]) -> \
+    Optional[Conflict]:
         for team in squares_claimed_by_team:
             for claimed_square in squares_claimed_by_team[team]:
                 if claimed_square == opponent and opp_claimed_square in squares_claimed_by_team[team][claimed_square]:
                     return self.Conflict({opponent, opp_claimed_square})
         return None
 
-    def extract_chain(self, row_idx: int, col_idx: int, stuck_unit_points=None, visited_points: Set[Tuple[int, int]]=None) -> Optional[List[Tuple[int, int]]]:
+    def extract_chain(self, row_idx: int, col_idx: int, stuck_unit_points=None,
+                      visited_points: Set[Tuple[int, int]] = None) -> Optional[List[Tuple[int, int]]]:
         """
         Returns a chain of units that must move in order, starting from the specified row_idx, col_idx.
         Returns None if the unit cannot move
@@ -305,6 +383,7 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tile Board")
 
+
 class InteractMode(Enum):
     ADD_MODE = 1
     ERASE_MODE = 2
@@ -313,11 +392,18 @@ class InteractMode(Enum):
 def main():
     board = Board.from_string(
         [
-            "GGGGGWG",
+            "GGGGGGG",
             "GGGGGGG",
             "GGGGGGG",
         ]
     )
+
+    board.tiles[1][0].unit = Unit(UnitType.SOLDIER, Direction.RIGHT, Team.ORANGE)
+    board.tiles[0][1].unit = Unit(UnitType.SOLDIER, Direction.RIGHT, Team.ORANGE)
+    board.tiles[1][1].unit = Unit(UnitType.SOLDIER, Direction.RIGHT, Team.ORANGE)
+    board.tiles[0][4].unit = Unit(UnitType.SOLDIER, Direction.LEFT, Team.APPLE)
+    board.tiles[1][4].unit = Unit(UnitType.SOLDIER, Direction.LEFT, Team.APPLE)
+    board.tiles[2][4].unit = Unit(UnitType.SOLDIER, Direction.LEFT, Team.APPLE)
 
     add_button = Button(400, 400, 50, 30, "Add", InteractMode.ADD_MODE)
     erase_button = Button(460, 400, 50, 30, "Erase", InteractMode.ERASE_MODE)
@@ -328,8 +414,6 @@ def main():
     running = True
     while running:
         screen.fill((255, 255, 255))
-
-        board.update()
 
         # Render the board
         board.render()
@@ -361,13 +445,14 @@ def main():
                     row = pos[1] // TILE_SIZE
                     if row < len(board.tiles) and col < len(board.tiles[0]):
                         if current_mode == InteractMode.ADD_MODE:
-                            if board.tiles[row][col].is_free():
+                            if board.tiles[row][col].is_free() and board.tiles[row][col].placeable:
                                 board.tiles[row][col].unit = Unit(UnitType.SOLDIER, Direction.RIGHT, Team.ORANGE)
                         elif current_mode == InteractMode.ERASE_MODE:
                             board.tiles[row][col].unit = None
 
-        pygame.time.delay(20)
+        pygame.time.delay(1000)
 
+        board.update()
 
     pygame.quit()
 
